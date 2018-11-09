497610599368c1c62e03b3c359bafc9574682434
clamcgui <clamcgui@indiana.edu>
2018-11-09 17:10:04 -0500

    Popups for adding/editing, Movable rows, Exportable data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // https://www.sice.indiana.edu/doc/career/2018-2019-career-search-guide/Career-Search-Guide-2018-2019.pdf
 
-angular.module('app', []).controller('main', function ($scope) {
+angular.module('app', ['ngPopup']).controller('main', function ($scope) {
     var vm = $scope;
 
     // generate state
@@ -14,72 +14,166 @@ angular.module('app', []).controller('main', function ($scope) {
         experience: []
     };
 
+    vm.export = false;
+
     load();
 
-    // general functions
+    // save/load
     vm.save = function () {
         localStorage.state = JSON.stringify(vm.state);
+
+        prepareExport(vm.state);
     };
 
     function load() {
-        if (localStorage.state)
+        if (localStorage.state) {
             vm.state = JSON.parse(localStorage.state);
+
+            prepareExport(vm.state);
+        }
+    }
+
+    // import/export
+    function prepareExport(data) {
+        var blob = new Blob([JSON.stringify(data, null, 2)], {
+            type: 'text/json'
+        });
+
+        vm.export = true;
+        
+        setTimeout(function() {
+            var a = document.querySelector('a[ng-if=export]');
+
+            a.download = 'resume.json';
+            a.href = window.URL.createObjectURL(blob);
+            a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+        }, 0);
     }
 
     // link functions
     vm.addLink = function () {
-        var link = linkFactory();
-        vm.state.links.push(link);
-        vm.editLink(link);
+        vm.adding = {
+            item: linkFactory()
+        };
+        vm.popups['Add Link'].viewPopup();
     };
 
-    vm.editLink = function (item) {
-        item.editing = angular.copy(item);
+    vm.addLinkCommit = function () {
+        vm.adding.item.type = determineLinkType(vm.adding.item.url);
+        vm.state.links.push(vm.adding.item);
+
+        vm.popups['Add Link'].hidePopup();
+
+        sortableRows();
     };
 
-    vm.saveLink = function (item, $index) {
-        vm.state.links[$index] = item.editing;
+    vm.editLink = function (item, $index) {
+        vm.editing = {
+            item: angular.copy(item),
+            $index: $index
+        };
+        vm.popups['Edit Link'].viewPopup();
     };
 
-    vm.cancelLink = function (item, $index) {
-        if (!Object.keys(item).some(x => x !== 'editing' && item[x]))
-            vm.state.links.splice($index, 1);
-        else
-            delete item.editing;
+    vm.editLinkCommit = function () {
+        vm.editing.item.type = determineLinkType(vm.editing.item.url);
+        vm.state.links[vm.editing.$index] = vm.editing.item;
+        vm.popups['Edit Link'].hidePopup();
     };
 
     vm.deleteLink = function ($index) {
-        if (confirm('Delete this link? Are you sure?'))
+        if (confirm('Delete this row? Are you sure?'))
             vm.state.links.splice($index, 1);
     };
 
+    // helper
+    function determineLinkType(url) {
+        var typeDict = {
+            'linkedin.com': 'LinkedIn',
+            'twitter.com': 'Twitter',
+            'github.com': 'GitHub'
+        };
+
+        for (var key of Object.keys(typeDict))
+            if (new RegExp('\\b' + key, 'i').test(url))
+                return typeDict[key];
+
+        return 'Other';
+    }
+
+    vm.linkTypes = {
+        'LinkedIn': 'fab fa-linkedin',
+        'Twitter': 'fab fa-twitter-square',
+        'GitHub': 'fab fa-github-square',
+        'Other': 'fas fa-globe-americas'
+    }
+
     // education functions
     vm.addEducation = function () {
-        var education = educationFactory();
-        vm.state.education.push(education);
-        vm.editEducation(education);
+        vm.adding = {
+            item: educationFactory()
+        };
+        vm.popups['Add Education'].viewPopup();
     };
 
-    vm.editEducation = function (item) {
-        item.editing = angular.copy(item);
+    vm.addEducationCommit = function () {
+        vm.state.education.push(vm.adding.item);
+
+        vm.popups['Add Education'].hidePopup();
+
+        sortableRows();
     };
 
-    vm.saveEducation = function (item, $index) {
-        vm.state.education[$index] = item.editing;
+    vm.editEducation = function (item, $index) {
+        vm.editing = {
+            item: angular.copy(item),
+            $index: $index
+        };
+        vm.popups['Edit Education'].viewPopup();
     };
 
-    vm.cancelEducation = function (item, $index) {
-        console.log(item);
+    vm.editEducationCommit = function () {
+        vm.state.education[vm.editing.$index] = vm.editing.item;
+        vm.popups['Edit Education'].hidePopup();
+    };
 
-        if (!Object.keys(item).some(x => x !== 'editing' && item[x]))
+    vm.deleteEducation = function ($index) {
+        if (confirm('Delete this row? Are you sure?'))
             vm.state.education.splice($index, 1);
-        else
-            delete item.editing;
     };
 
     // experience functions
     vm.addExperience = function () {
+        vm.adding = {
+            item: experienceFactory()
+        };
+        vm.popups['Add Experience'].viewPopup();
+    };
 
+    vm.addExperienceCommit = function () {
+        vm.state.experience.push(vm.adding.item);
+
+        vm.popups['Add Experience'].hidePopup();
+
+        sortableRows();
+    };
+
+    vm.editExperience = function (item, $index) {
+        vm.editing = {
+            item: angular.copy(item),
+            $index: $index
+        };
+        vm.popups['Edit Experience'].viewPopup();
+    };
+
+    vm.editExperienceCommit = function () {
+        vm.state.experience[vm.editing.$index] = vm.editing.item;
+        vm.popups['Edit Experience'].hidePopup();
+    };
+
+    vm.deleteExperience = function ($index) {
+        if (confirm('Delete this row? Are you sure?'))
+            vm.state.experience.splice($index, 1);
     };
 
     // factories
@@ -95,7 +189,7 @@ angular.module('app', []).controller('main', function ($scope) {
             degree: '',
             school: '',
             location: '',
-            date: ''
+            timespan: ''
         };
     }
 
@@ -104,8 +198,49 @@ angular.module('app', []).controller('main', function ($scope) {
             title: '',
             organization: '',
             location: '',
-            date: '',
+            timespan: '',
             blurbs: []
         };
+    }
+
+    // sortable rows
+    function sortableRows() {
+        setTimeout(function () {
+            var s = sortable('[ng-sortable]', {
+                forcePlaceholderSize: true,
+                handle: '[ng-sort]'
+            });
+
+            for (var e of s)
+                e.addEventListener('sortupdate', sortUpdate);
+        }, 0);
+
+        function sortUpdate(e) {
+            // Determine moved node and new position
+            var oldIndex = e.detail.origin.index;
+            var newIndex = e.detail.destination.index;
+
+            var $repeater = $(e.target).children('[ng-repeat]:first-of-type'); // get the repeater that references the collection
+
+            var collectionName = /[^ ]+ in ([^ ]+)/.exec($repeater.attr('ng-repeat'))[1]; // get the name of the collection
+            var collection = getVariable(collectionName); // get the actual collection
+
+            var item = collection.splice(oldIndex, 1)[0]; // remove and grab item
+            collection.splice(newIndex, 0, item); // move item to new location
+
+            vm.$apply();
+        }
+    }
+
+    sortableRows();
+
+    // helpers
+    function getVariable(x) { // get a scope variable by its name, even if it's nested, e.g. "state.education"
+        var result = vm;
+
+        for (var key of x.split('.'))
+            result = result[key];
+
+        return result;
     }
 });
